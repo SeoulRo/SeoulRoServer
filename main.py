7a969b32f79066277d73b0d8167d7ca7db4a2ad5
KyooSik Lee <kyoosik.lee@gmail.com>
2022-08-24 17:54:58 +0900

    feat: add sensor data api

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
 import populartimes
+
+client = MongoClient("localhost", 27017)
 
 GOOGLE_MAPS_API_KEY = "AIzaSyCTV39OY2_5wmq6ZNB9CAeEvj6VdRZCYt8"
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
 
 @app.get("/populartimes/{placeId}")
 async def get_popular_times(placeId: str):
     print(placeId)
     return populartimes.get_id(GOOGLE_MAPS_API_KEY, placeId)
+
+@app.get("/sensors")
+async def get_sensor_data():
+    db = client.seoulro
+    collection = db['sensor_data']
+    return list(collection.find({}))[0]
